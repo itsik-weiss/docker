2a677343afcd96a1965022ce50a1fdc6e6ca2345
itsikweiss1@gmail.com <itsikweiss1@gmail.com>
2024-06-03 20:23:40 +0300

    run docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-HOST_IP=$(hostname -I | awk '{print $1}')
-export HOST_IP
+public_ip=$(curl -s https://api.ipify.org)
+export HOST_IP=$public_ip
 
-docker-compose up
+if [[ -z "$HOST_IP" ]]; then
+  echo "Failed to retrieve the public IP address"
+  exit 1
+fi
+
+sudo -E docker-compose up --build
