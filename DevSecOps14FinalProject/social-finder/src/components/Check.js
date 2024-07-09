bee9b88ff1748eaccc26c9b8ecf811c9ab69bd6f
itsikweiss1@gmail.com <itsikweiss1@gmail.com>
2024-07-09 15:35:57 +0300

    lb

--- DevSecOps14FinalProject/social-finder/src/components/Check.js
+++ DevSecOps14FinalProject/social-finder/src/components/Check.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState } from "react";
 import './Check.css';
 
-const API_URL = 'http://34.27.92.249:5001/api';
+const API_URL = 'http://34.45.58.51:5001/api';
 
 function Check() {
   const [name, setName] = useState("");
